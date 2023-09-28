d45d7417c067da2c6f421e97780c1c19d0dc1f92
“nicoleng” <nicoleng123@gmail.com>
2023-09-28 23:09:18 +0000

    initial test training

--- tutorial_dataset.py
+++ tutorial_dataset.py
@@ -8,7 +8,7 @@ from torch.utils.data import Dataset
 class MyDataset(Dataset):
     def __init__(self):
         self.data = []
-        with open('./training/fill50k/prompt.json', 'rt') as f:
+        with open('./data/fill50k/prompt.json', 'rt') as f:
             for line in f:
                 self.data.append(json.loads(line))
 
@@ -22,12 +22,20 @@ class MyDataset(Dataset):
         target_filename = item['target']
         prompt = item['prompt']
 
-        source = cv2.imread('./training/fill50k/' + source_filename)
-        target = cv2.imread('./training/fill50k/' + target_filename)
+        source = cv2.imread('./data/fill50k/' + source_filename)
+        target = cv2.imread('./data/fill50k/' + target_filename)
 
-        # Do not forget that OpenCV read images in BGR order.
-        source = cv2.cvtColor(source, cv2.COLOR_BGR2RGB)
-        target = cv2.cvtColor(target, cv2.COLOR_BGR2RGB)
+        if source is None:
+            print(f"Failed to load source image at index {idx} with filename {source_filename}")
+        else:
+            # Do not forget that OpenCV read images in BGR order.
+            source = cv2.cvtColor(source, cv2.COLOR_BGR2RGB)
+
+        if target is None:
+            print(f"Failed to load target image at index {idx} with filename {target_filename}")
+        else:
+            target = cv2.cvtColor(target, cv2.COLOR_BGR2RGB)
+
 
         # Normalize source images to [0, 1].
         source = source.astype(np.float32) / 255.0
